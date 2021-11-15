4325831b11691fceb721706849f18c540dbd051b
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2021-11-14 23:57:56 -0300

    Aula 16

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -12,13 +12,18 @@ export class OutputPropertyComponent implements OnInit {
 
   @Output() mudouValor = new EventEmitter();
 
+  @ViewChild('campoInput', {static: false}) campoValorInput!: ElementRef;
+
   incrementa(){
-    this.valor++;
+    //console.log(this.campoValorInput.nativeElement.value);
+    //this.valor++;
+    this.campoValorInput.nativeElement.valuer++;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
   decrementa(){
-    this.valor--;
+    //this.valor--;
+    this.campoValorInput.nativeElement.value--;
     this.mudouValor.emit({ novoValor: this.valor });
   }
 
